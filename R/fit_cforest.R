83fc49ffb4e982f4aac409afa74b4a9dab2a71ed
ielbadisy <elbadisyimad@gmail.com>
2025-06-26 16:29:15 +0100

    add tuner for cforest

--- R/fit_cforest.R
+++ R/fit_cforest.R
@@ -89,3 +89,87 @@ cv_results_cforest <- cv_survlearner(
 
 # --- Print Results ---
 print(cv_results_cforest)
+
+
+
+#---------------- tune_cforest()
+
+tune_cforest <- function(formula, data, times,
+                         param_grid = expand.grid(
+                           ntree = c(100, 300),
+                           mtry = c(2, 3),
+                           mincriterion = c(0, 0.95),
+                           fraction = c(0.5, 0.632)
+                         ),
+                         metrics = c("cindex", "ibs"),
+                         folds = 5, seed = 123, ...) {
+
+  purrr::pmap_dfr(param_grid, function(ntree, mtry, mincriterion, fraction) {
+    cv_results <- cv_survlearner(
+      formula = formula,
+      data = data,
+      fit_fun = fit_cforest,
+      pred_fun = predict_cforest,
+      times = times,
+      metrics = metrics,
+      folds = folds,
+      seed = seed,
+      ntree = ntree,
+      mtry = mtry,
+      mincriterion = mincriterion,
+      fraction = fraction,
+      ...
+    )
+
+    summary <- cv_summary(cv_results)
+    tibble::tibble(ntree, mtry, mincriterion, fraction) |>
+      bind_cols(tidyr::pivot_wider(summary[, c("metric", "mean")], names_from = metric, values_from = mean))
+  }) |> arrange(dplyr::across(any_of(metrics[1])))
+}
+
+
+## test tuner
+
+res_cforest <- tune_cforest(
+  formula = Surv(time, status) ~ age + celltype + karno,
+  data = veteran,
+  times = c(100, 200, 300),
+  param_grid = expand.grid(
+    ntree = c(100, 300),
+    mtry = c(2, 3),
+    mincriterion = c(0, 0.95),
+    fraction = c(0.5, 0.632)
+  ),
+  metrics = c("cindex", "ibs"),
+  folds = 3
+)
+
+print(res_cforest)
+
+
+### extract best row
+best_row <- res_cforest |>
+  arrange(desc(cindex)) |>  # or arrange(ibs) if optimizing for IBS
+  slice(1)
+best_row
+
+
+### re-fit the final model
+final_model_cforest <- fit_cforest(
+  formula = Surv(time, status) ~ age + celltype + karno,
+  data = veteran,
+  ntree = best_row$ntree,
+  mtry = best_row$mtry,
+  mincriterion = best_row$mincriterion,
+  fraction = best_row$fraction
+)
+
+
+### evaluate the final model
+evaluate_survlearner(
+  model = final_model_cforest,
+  metrics = c("cindex", "ibs", "iae", "ise"),
+  times = c(100, 200, 300)
+)
+
+
