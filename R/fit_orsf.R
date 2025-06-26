557d25a6d9332fdf376355985c50624a2b47c36c
ielbadisy <elbadisyimad@gmail.com>
2025-06-26 16:30:22 +0100

    add tuner for orsf

--- R/fit_orsf.R
+++ R/fit_orsf.R
@@ -68,3 +68,78 @@ library(survival)
 mod_orsf <- fit_orsf(Surv(time, status) ~ age + ph.ecog, data = lung)
 pred_orsf <- predict_orsf(mod_orsf, newdata = lung[1:5, ], times = c(100, 200, 300))
 print(round(pred_orsf, 3))
+
+cv_results_orsf <- cv_survlearner(
+  formula = Surv(time, status) ~ age + ph.ecog,
+  data = lung,
+  fit_fun = fit_orsf,
+  pred_fun = predict_orsf,
+  times = c(100, 200, 300),
+  metrics = c("cindex", "ibs"),
+  folds = 5,
+  seed = 42
+)
+
+cv_summary(cv_results_orsf)
+cv_plot(cv_results_orsf)
+
+
+#----------- add tuner
+
+tune_orsf <- function(formula, data, times,
+                      param_grid = expand.grid(
+                        n_tree = c(100, 300),
+                        mtry = c(2, 3),
+                        min_events = c(5, 10)
+                      ),
+                      metrics = c("cindex", "ibs"),
+                      folds = 5,
+                      seed = 123,
+                      ...) {
+
+  purrr::pmap_dfr(param_grid, function(n_tree, mtry, min_events) {
+    cv_results <- cv_survlearner(
+      formula = formula,
+      data = data,
+      fit_fun = fit_orsf,
+      pred_fun = predict_orsf,
+      times = times,
+      metrics = metrics,
+      folds = folds,
+      seed = seed,
+      n_tree = n_tree,
+      mtry = mtry,
+      n_split = min_events,
+      ...
+    )
+
+    summary <- cv_summary(cv_results)
+
+    tibble::tibble(
+      n_tree = n_tree,
+      mtry = mtry,
+      min_events = min_events
+    ) |>
+      dplyr::bind_cols(
+        tidyr::pivot_wider(summary[, c("metric", "mean")],
+                           names_from = metric, values_from = mean)
+      )
+  }) |> dplyr::arrange(dplyr::across(any_of(metrics[1])))
+}
+
+
+
+res_orsf <- tune_orsf(
+  formula = Surv(time, status) ~ age + ph.ecog,
+  data = lung,
+  times = c(100, 200, 300),
+  param_grid = expand.grid(
+    n_tree = c(100, 300),
+    mtry = c(1, 2),             # o larger than number of covariates
+    min_events = c(5, 10)
+  ),
+  metrics = c("cindex", "ibs"),
+  folds = 3
+)
+
+print(res_orsf)
