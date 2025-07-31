477a46e9b09d9a1440e370279345ccca9ec5f1f2
ielbadisy <elbadisyimad@gmail.com>
2025-07-31 12:23:34 +0100

    test fit/predict/CV

--- R/fit_aftgee.R
+++ R/fit_aftgee.R
@@ -49,26 +49,25 @@ predict_aftgee <- function(object, newdata, times = NULL, ...) {
 
 
 library(aftgee)
-set.seed(123)
 
-# simulated example
-datgen <- function(n = 100, tau = 0.3, dim = 2) {
-  x1 <- rbinom(dim * n, 1, 0.5)
-  x2 <- rnorm(dim * n)
-  e <- c(t(exp(MASS::mvrnorm(n = n, mu = rep(0, dim), Sigma = tau + (1 - tau) * diag(dim)))))
-  tt <- exp(2 + x1 + x2 + e)
-  cen <- runif(n, 0, 100)
-  data.frame(Time = pmin(tt, cen), status = 1 * (tt < cen),
-             x1 = x1, x2 = x2, id = rep(1:n, each = dim))
-}
-dat <- datgen(n = 50, dim = 1)
+library(survival)
+
+mod <- fit_aftgee(Surv(time, status) ~ trt + karno + age,
+                 data = veteran)
+
+
+
+pred <- predict_aftgee(mod, newdata = veteran, times = c(0, 100, 300, 500))
+pred
+
+
 
 cv_survlearner(
-  formula = Surv(Time, status) ~ x1 + x2,
-  data = dat,
+  formula = Surv(time, status) ~ trt + karno + age,
+  data = veteran,
   fit_fun = fit_aftgee,
   pred_fun = predict_aftgee,
-  times = c(10, 20, 40),
+  times = c(0, 20, 40),
   metrics = c("cindex", "ibs"),
   folds = 5
 )
