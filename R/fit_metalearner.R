9adf852640b06f8efcc611c8b54390260cb1c683
ielbadisy <elbadisyimad@gmail.com>
2025-08-02 11:06:44 +0100

    more consistant test for metalearner

--- R/fit_metalearner.R
+++ R/fit_metalearner.R
@@ -1,6 +1,7 @@
 fit_metalearner <- function(base_preds, time, status, times,
-                                       base_models, formula, data) {
+                            base_models, formula, data) {
   stopifnot(is.list(base_preds), is.list(base_models), is.data.frame(data))
+
   base_preds <- lapply(base_preds, function(x) {
     if (!is.matrix(x)) as.matrix(x) else x
   })
@@ -19,21 +20,21 @@ fit_metalearner <- function(base_preds, time, status, times,
     weight_matrix[, j] <- w
   }
 
-  structure(
-    list(
-      weights = weight_matrix,
-      times = times,
-      learners = learners,
-      base_models = base_models,
-      formula = formula,
-      data = data
-    ),
-    class = "metalearner_nnls_multi",
-    engine = "metalearner_nnls_multi"
-  )
+  structure(list(
+    weights = weight_matrix,
+    base_models = base_models,
+    base_preds = base_preds,
+    formula = formula,
+    data = data,
+    time = time,
+    status = status,
+    learners = learners  # ✅ FIXED: store learners
+  ), class = "metalearner", learner = "metalearner", engine = "metalearner")
 }
+
+
 predict_metalearner <- function(model, newdata, times) {
-  stopifnot(inherits(model, "metalearner_nnls_multi"))
+  stopifnot(inherits(model, "metalearner"))
 
   learners <- model$learners
   base_models <- model$base_models
@@ -66,7 +67,7 @@ predict_metalearner <- function(model, newdata, times) {
 }
 
 plot_metalearner_weights <- function(model) {
-  stopifnot(inherits(model, "metalearner_nnls_multi"))
+  stopifnot(inherits(model, "metalearner"))
   library(ggplot2); library(tidyr); library(dplyr)
 
   W <- as.data.frame(model$weights)
@@ -82,82 +83,58 @@ plot_metalearner_weights <- function(model) {
     theme_minimal(base_size = 14)
 }
 
-times <- c(1, 2, 3, 4, 5, 7)
-
-mod_aareg  <- fit_aareg(Surv(time, status == 9) ~ sex + diabetes + chf + vf, data = sTRACE)
-mod_rpart  <- fit_rpart(Surv(time, status == 9) ~ sex + diabetes + chf + vf, data = sTRACE)
-mod_ranger <- fit_ranger(Surv(time, status == 9) ~ sex + diabetes + chf + vf, data = sTRACE)
-
-base_preds <- list(
-  aareg  = predict_aareg(mod_aareg, sTRACE, times),
-  rpart  = predict_rpart(mod_rpart, sTRACE, times),
-  ranger = predict_ranger(mod_ranger, sTRACE, times)
-)
-
-meta_model <- fit_metalearner(
-  base_preds, sTRACE$time, sTRACE$status == 9, times,
-  base_models = list(aareg = mod_aareg, rpart = mod_rpart, ranger = mod_ranger),
-  formula = mod_aareg$formula, data = sTRACE
-)
-
-eval_all <- list(
-  aareg  = evaluate_survlearner(mod_aareg,  metrics = c("cindex", "ibs", "iae", "ise"), times = times),
-  rpart  = evaluate_survlearner(mod_rpart,  metrics = c("cindex", "ibs", "iae", "ise"), times = times),
-  ranger = evaluate_survlearner(mod_ranger, metrics = c("cindex", "ibs", "iae", "ise"), times = times),
-  meta   = evaluate_survlearner(meta_model, metrics = c("cindex", "ibs", "iae", "ise"), times = times)
-)
-
-do.call(rbind, eval_all)
-plot_metalearner_weights(meta_model)
 
 
+## TEST -------
 
-#********
+library(survival)
+data(veteran)
 
+formula <- Surv(time, status) ~ age + karno + trt + diagtime + celltype
+times <- c(30, 60, 90, 120, 180)
 
-
-times <- c(1, 3, 5)
-formula <- Surv(time, status == 9) ~ sex + diabetes + chf + vf
-
-mod_aareg   <- fit_aareg(formula, data = sTRACE)
-mod_rpart   <- fit_rpart(formula, data = sTRACE)
-mod_ranger  <- fit_ranger(formula, data = sTRACE)
-mod_cforest <- fit_cforest(formula, data = sTRACE)
-mod_xgboost <- fit_xgboost(formula, data = sTRACE)
+mod_aareg   <- fit_aareg(formula, data = veteran)
+mod_rpart   <- fit_rpart(formula, data = veteran)
+mod_ranger  <- fit_ranger(formula, data = veteran)
+mod_cforest <- fit_cforest(formula, data = veteran)
+mod_xgboost <- fit_xgboost(formula, data = veteran)
 
 base_preds <- list(
-  aareg   = predict_aareg(mod_aareg, sTRACE, times),
-  rpart   = predict_rpart(mod_rpart, sTRACE, times),
-  ranger  = predict_ranger(mod_ranger, sTRACE, times),
-  cforest = predict_cforest(mod_cforest, sTRACE, times),
-  xgboost = predict_xgboost(mod_xgboost, sTRACE, times))
+  aareg   = predict_aareg(mod_aareg, veteran, times),
+  rpart   = predict_rpart(mod_rpart, veteran, times),
+  ranger  = predict_ranger(mod_ranger, veteran, times),
+  cforest = predict_cforest(mod_cforest, veteran, times),
+  xgboost = predict_xgboost(mod_xgboost, veteran, times)
+)
 
 base_models <- list(
-  aareg = mod_aareg, rpart = mod_rpart, ranger = mod_ranger, cforest = mod_cforest,
-  xgboost = mod_xgboost )
-
-meta_model <- fit_metalearner(
-  base_preds, sTRACE$time, sTRACE$status == 9, times,
-  base_models = base_models,
-  formula = formula, data = sTRACE
+  aareg   = mod_aareg,
+  rpart   = mod_rpart,
+  ranger  = mod_ranger,
+  cforest = mod_cforest,
+  xgboost = mod_xgboost
 )
 
-eval_all <- lapply(base_models, function(mod) {
-  evaluate_survlearner(mod, metrics = c("cindex", "ibs", "iae", "ise"), times = times)
-})
-eval_all$meta <- evaluate_survlearner(meta_model, metrics = c("cindex", "ibs", "iae", "ise"), times = times)
+meta_model <- fit_metalearner(
+  base_preds = base_preds,
+  time = veteran$time,
+  status = veteran$status,
+  times = times,
+  base_models = base_models,
+  formula = formula,
+  data = veteran
+)
 
-do.call(rbind, eval_all)
+# Prediction
+pred_meta <- predict_metalearner(meta_model, newdata = veteran[1:5, ], times = times)
+print(round(pred_meta, 3))
+
+# Plot
 plot_metalearner_weights(meta_model)
 
-eval_all
 
-library(dplyr)
-library(tidyr)
-library(purrr)
 
-tibble(
-  learner = names(eval_all),
-  cindex = map_dbl(eval_all, ~ .x$value[.x$metric == "iae"])
-) %>%
-  arrange(desc(cindex))
+eval_meta <- evaluate_survlearner(meta_model, metrics = c("cindex", "ibs", "iae", "ise"), times = times)
+
+
+eval_meta
