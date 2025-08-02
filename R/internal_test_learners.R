f430a26a80cd4962494e49f5c5aa45422d99650c
ielbadisy <elbadisyimad@gmail.com>
2025-08-02 11:03:33 +0100

    test coxph

--- R/internal_test_learners.R
+++ R/internal_test_learners.R
@@ -1,10 +1,10 @@
 
 learners <- tibble::tibble(
-  learner_name = c("aareg", "aftgee", "mboost", "ranger", "orsf", "glmnet", "flexsurv", "bnnsurv", "coxaalen", "rstpm2", "rpart", "rfsrc", "survivalsvm", "xgboost"),
-  fit_fun = list(fit_aareg, fit_aftgee, fit_mboost, fit_ranger, fit_orsf, fit_glmnet, fit_flexsurv, fit_bnnsurv, fit_coxaalen, fit_rstpm2, fit_rpart, fit_rfsrc, fit_survivalsvm,
+  learner_name = c("aareg", "aftgee", "mboost", "ranger", "orsf", "glmnet", "flexsurv", "bnnsurv", "coxaalen", "rstpm2", "rpart", "rfsrc", "survivalsvm", "xgboost", "coxph"),
+  fit_fun = list(fit_aareg, fit_aftgee, fit_mboost, fit_ranger, fit_orsf, fit_glmnet, fit_flexsurv, fit_bnnsurv, fit_coxaalen, fit_rstpm2, fit_rpart, fit_rfsrc, fit_survivalsvm, fit_coxph,
                  fit_xgboost),
   pred_fun = list(predict_aareg, predict_aftgee, predict_mboost, predict_ranger, predict_orsf, predict_glmnet, predict_flexsurv, predict_bnnsurv, predict_coxaalen,
-                  predict_rstpm2, predict_rpart, predict_rfsrc, predict_survivalsvm, predict_xgboost)
+                  predict_rstpm2, predict_rpart, predict_rfsrc, predict_survivalsvm, predict_xgboost, predict_coxph)
 )
 
 results_by_learner <- purrr::pmap_dfr(
