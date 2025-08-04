722e8bab9cb2858da9745fb3cdd6fa6665ff10b9
ielbadisy <elbadisyimad@gmail.com>
2025-08-04 14:56:40 +0100

    refactor AFT ensemble wrapper; ensure modularity and better error handling

--- R/fit_aftgee.R
+++ R/fit_aftgee.R
@@ -22,7 +22,12 @@ fit_aftgee <- function(formula, data, corstr = "independence", ...) {
 
 
 predict_aftgee <- function(object, newdata, times = NULL, ...) {
-  stopifnot(object$learner == "aftgee")
+
+
+  if (!is.null(object$learner) && object$learner != "aftgee") {
+    warning("Object passed to predict_aftgee() may not come from fit_aftgee().")
+  }
+
   stopifnot(requireNamespace("aftgee", quietly = TRUE))
 
   beta <- object$model$coef.res
@@ -30,20 +35,18 @@ predict_aftgee <- function(object, newdata, times = NULL, ...) {
   log_time_pred <- as.numeric(X %*% beta)
 
   if (is.null(times)) {
-    # use quantiles or range from newdata to set a reasonable time grid
     t_max <- max(newdata[[all.vars(object$formula[[2]])[1]]], na.rm = TRUE)
     times <- seq(1, t_max, length.out = 20)
-  }
+    }
 
 
   # approximate survival using lognormal-like assumption
-  surv_mat <- sapply(times, function(t) {
+  survmat <- sapply(times, function(t) {
     1 - pnorm((log(t) - log_time_pred))  # approximate S(t) from log-T model
   })
 
-  colnames(surv_mat) <- paste0("t=", times)
-  rownames(surv_mat) <- paste0("ID_", seq_len(nrow(newdata)))
-  as.data.frame(surv_mat)
+  colnames(survmat) <- paste0("t=", times)
+  as.data.frame(survmat)
 }
 
 
@@ -57,7 +60,7 @@ mod <- fit_aftgee(Surv(time, status) ~ trt + karno + age,
 
 
 
-pred <- predict_aftgee(mod, newdata = veteran, times = c(0, 100, 300, 500))
+pred <- predict_aftgee(mod, newdata = veteran, times = 0:100)
 pred
 
 
