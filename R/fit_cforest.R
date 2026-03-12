6a726983972dc4adb09e7040b60964f2a1b30e1f
ielbadisy <elbadisyimad@gmail.com>
2026-03-12 13:22:17 +0000

    Fix cforest tuning order for loss metrics

--- R/fit_cforest.R
+++ R/fit_cforest.R
@@ -220,7 +220,11 @@ tune_cforest <- function(formula, data, times,
       )
   })
 
-  results <- results |> dplyr::arrange(dplyr::desc(!!rlang::sym(metrics[1])))
+  if (metrics[1] %in% c("cindex", "auc", "accuracy")) {
+    results <- results |> dplyr::arrange(dplyr::desc(!!rlang::sym(metrics[1])))
+  } else {
+    results <- results |> dplyr::arrange(!!rlang::sym(metrics[1]))
+  }
 
   if (!refit_best) {
     class(results) <- c("tuned_surv", class(results))
